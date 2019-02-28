950b491b35a8129579a07d9513ca6e08b2192453
Andy Ly <andywindsong@gmail.com>
2019-02-28 12:31:15 -0600

    sql connections

--- VS_Projects/ADONetConnected/ADONetConnected/Program.cs
+++ VS_Projects/ADONetConnected/ADONetConnected/Program.cs
@@ -17,7 +17,70 @@ namespace ADONetConnected
             // we need a data provider - use NuGet package System.Data.sqlClient
             // for SQL server connections
 
-            SqlConnection connString = "Server=tcp:ly1902sql.database.windows.net,1433;Initial Catalog=Chinook;Persist Security Info=False;User ID={your_username};Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            var connString = SecretConfiguration.ConnectionString;
+            //var connection = new SqlConnection(connString);
+
+            Console.WriteLine("Enter condition: ");
+            var condition = Console.ReadLine();
+            // e.g. MovieId < 2
+            var commString = $"SELECT * FROM Movie.Movie Where {condition}";
+
+            // CAREFUL OF SQL INJECTION!!!
+            // user could enter "1 = 1; DROP TABLE Movie.Movie;" and I drop table.
+            // solution: sanitize and validate all user input
+
+            // for connected architecture
+
+            // we should be catching exceptions
+            using (var connection = new SqlConnection(connString))
+            {
+                // 1. open the connection
+                connection.Open();
+
+                // 2. execute query
+                var commString2 = "SELECT * FROM Movie.Movie;";
+                using (var command = new SqlCommand(commString2, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        // we have a command.ExecuteReader which returns a DataReader
+                        //  For SELECT queries
+                        // we have a command.ExecuteQuery which just returns num
+                        //  of rows affected for DELETE, INSERT, etc
+
+                    //3. process the results
+                        if(reader.HasRows)
+                        {
+                            // reader.read advances the cursor through the results, one row at a time
+                            // the results are coming in to the computer's network buffer
+                            // and DataReader is reading them each as soon as they come in
+                            // (network are slower compared to code) 
+                            while(reader.Read())
+                            {
+                                object id = reader["MovieId"];
+                                object title = reader["Title"];
+                                // object fullTitle = reader[5]; //5th col, 0 takes something up it seems?
+                                string fullTitle = reader.GetString(5); //5th col
+
+                                // i could do downcasting and instantiate some Movie class
+                                // or just print results
+
+                                Console.WriteLine($"Movie #{id}: {fullTitle}");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("No rows returned");
+
+                        }
+
+                        // 4. close the connection
+                        connection.Close();
+                    }
+                }
+            }
+            //connection.Open();
+            Console.ReadKey();
         }
     }
 }
