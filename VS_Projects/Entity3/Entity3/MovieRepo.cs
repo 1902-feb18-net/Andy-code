50dbb3d1135957d5a5b033abdbcc1aa504f24ddd
Andy Ly <andywindsong@gmail.com>
2019-03-01 01:18:02 -0600

    a bit more on movierepo

--- VS_Projects/Entity3/Entity3/MovieRepo.cs
+++ VS_Projects/Entity3/Entity3/MovieRepo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Entity3Database;
 
@@ -8,6 +9,8 @@ namespace Entity3
     // here we implement our interface IMovieRepo
     class MovieRepo : IMovieRepo
     {
+        public static MoviesContext dbContext { get; set; }
+
         public void AddMovie(Movie movie)
         {
             throw new NotImplementedException();
@@ -18,29 +21,36 @@ namespace Entity3
             throw new NotImplementedException();
         }
 
-        public IEnumerable<Genre> GetAllGenres()
+        public void UpdateMovie(Movie movie)
         {
             throw new NotImplementedException();
+        }
+
+        public IEnumerable<Genre> GetAllGenres()
+        {
+            //throw new NotImplementedException();
+            return dbContext.Genre.ToList();
         }
 
         public IEnumerable<Movie> GetAllMovies()
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return dbContext.Movie.ToList();
         }
 
         public Movie GetMovieById(int id)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return dbContext.Movie
+                .Single(movie => movie.MovieId == id);
         }
 
         public IEnumerable<Movie> GetMoviesByGenre(Genre genre)
         {
-            throw new NotImplementedException();
-        }
-
-        public void UpdateMovie(Movie movie)
-        {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return dbContext.Movie
+                .ToList()
+                .Where(movie => movie.Genre == genre);
         }
     }
 }
