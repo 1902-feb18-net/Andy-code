b80a41f6f2335c9496920785b7f9106863b96e01
Andy Ly <andywindsong@gmail.com>
2019-03-06 14:17:38 -0600

    setting up mvc

---
+++ VS_Projects/mvc/mvc/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using mvc.Models;
+
+namespace mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // each method in controller is going to be called an action method
+        // handles the response that happens on the page
+        public IActionResult Index()
+        {
+            var user = new User { Username = "Fred" };
+            return View(user);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ VS_Projects/mvc/mvc/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace mvc
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // in VS we have "IIS Express" Web Server (crucial for HTTP lifecycle)
+            // is a server that knows how to take http requests and send back a response
+            // we servers are software that listen on ports
+            // for data sent over internet, especially HTTP
+            // our app runs inside/alongside that web server
+            // technically we have another web server inside that is a container directly for this 
+            // application called Kestrel (cross platform)
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        // creates a webhost that has a startup, what matters is the Startup
+        // ASP.NET creates a webhost and runs it, using configs given by a startup class
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
